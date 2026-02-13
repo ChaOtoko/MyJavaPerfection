52370bd6d24762e12e64c6b4de3edbdc11c34028
Matthew Gavigan <matthew@none.com>
2026-02-13 22:05:17 +0000

    Corrected the library reference in basicRun.sh

--- basicRun.sh
+++ basicRun.sh
@@ -1,3 +1,3 @@
 #Add any options/flags onto the end of this line to change the input/output.
 #For example I'd start with --help
-java -cp target/classes:lib org.test.seven_twenty.Main
+java -cp target/classes:lib/* org.test.seven_twenty.Main
